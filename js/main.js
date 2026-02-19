faf09f1f4d2aec6b4c17d8a79e74efd23ba4d704
hmaier-dev <hmaier-dev@gitub.com>
2026-02-19 12:29:41 +0100

    Einfärbung der verschiedenen Linien nach Farbschema was bei Siteforum gefunden wurde: https://abrmd.siteforum.com/de/app/webtools/messages.widget?design=1&navigation=0&action=overview&scheduled=1

--- js/main.js
+++ js/main.js
@@ -181,45 +181,80 @@ function parseSiteFormResponse(raw) {
 
 function renderTrafficCards(data) {
 
-  const html = data.map(item => `
-      <div class="traffic-card">
+  const danger = `<svg xmlns="http://www.w3.org/2000/svg" height="" viewBox="0 -960 960 960" width="" fill="#000000"><path d="M480-79q-16 0-30.5-6T423-102L102-423q-11-12-17-26.5T79-480q0-16 6-31t17-26l321-321q12-12 26.5-17.5T480-881q16 0 31 5.5t26 17.5l321 321q12 11 17.5 26t5.5 31q0 16-5.5 30.5T858-423L537-102q-11 11-26 17t-31 6Zm0-361q17 0 28.5-11.5T520-480v-160q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640v160q0 17 11.5 28.5T480-440Zm0 120q17 0 28.5-11.5T520-360q0-17-11.5-28.5T480-400q-17 0-28.5 11.5T440-360q0 17 11.5 28.5T480-320Z"/></svg>`
 
-          <div>
-              <div class="card-header">
-                  <div class="line-badge">${item.linie}</div>
-                  <div class="warning-icon">⚠</div>
-              </div>
 
-              <div class="route">
-                  <div class="route-from">${item.von}</div>
-                  <div class="route-arrow">→</div>
-                  <div class="route-to">${item.nach}</div>
-                  <div class="route-direction">(Einfache Richtung)</div>
-              </div>
+  const html = data.map(item =>  {
 
-              <div class="section">
-                  <div class="section-title">Zeitraum:</div>
-                  <div>${item.zeitraum}</div>
-              </div>
+    const key = item.linie.toLowerCase().replace(/\s+/g, "");
+    const color = lineColors[key] || "#999"; // fallback
+    console.log(`Karte für Linie ${key} wird gebaut.`)
 
-              <div class="section">
-                  <div class="section-title">Betroffener Abschnitt:</div>
-                  <div>${item.betroffener_abschnitt.von} bis ${item.betroffener_abschnitt.nach}</div>
-              </div>
-          </div>
 
-          <div class="card-footer">
-              Meldung vom ${item.meldung}
-          </div>
+    return `
+    <div class="traffic-card">
 
-      </div>
-  `).join("");
+        <div>
+            <div class="card-header">
+                <div class="line-badge" style="background-color: ${color}">${item.linie}</div>
+                <div class="warning-icon">${danger}</div>
+            </div>
+
+            <div class="route">
+                <div class="route-from">${item.von}</div>
+                <div class="route-arrow">→</div>
+                <div class="route-to">${item.nach}</div>
+                <div class="route-direction">(Einfache Richtung)</div>
+            </div>
+
+            <div class="section">
+                <div class="section-title">Zeitraum:</div>
+                <div>${item.zeitraum}</div>
+            </div>
+
+            <div class="section">
+                <div class="section-title">Betroffener Abschnitt:</div>
+                <div>${item.betroffener_abschnitt.von} bis ${item.betroffener_abschnitt.nach}</div>
+            </div>
+        </div>
+
+        <div class="card-footer">
+            Meldung vom ${item.meldung}
+        </div>
+
+    </div>
+  `;}).join("");
   return html
 
 }
 
 // --------------------------------------------------------------------
 // The action starts here
+
+
+
+const lineColors = {
+  rb75: "#b17f4a",
+  re17: "#36a9e1",
+  rb20: "#95c11f",
+  rb59: "#d47f8f",
+  rb51: "#804a96",
+  re19: "#95c11f",
+  re15: "#2aaae2",
+  re9: "#000",
+  re16: "#2e4c9c",
+  re10: "#52ae32",
+  "30": "#000",
+  re18: "#000",
+  rb44: "#d47f8f",
+  rb25: "#52ae32",
+  rb48: "#2d847e",
+  rb77: "#73542f",
+  re8: "#000",
+  re28: "#000",
+  s7: "#000",
+  rb57: "#000"
+};
 
 
 const verkehrslage_url = "https://abrmd.siteforum.com/de/app/webtools/messages.widget?design=0&navigation=0&action=overview&scheduled=0";
