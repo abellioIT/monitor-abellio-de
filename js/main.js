fd872c89e5dd841edc9b86626d7d2338fd3fa528
hmaier-dev <hmaier-dev@gitub.com>
2026-02-23 16:25:24 +0100

    Aufräumen im code

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
-function getParameterByName(e) {
-    var i = RegExp("[\\?&]" + (e = e.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]")) + "=([^&#]*)").exec(
-        location.search
-    );
-    return null !== i && decodeURIComponent(i[1].replace(/\+/g, " "));
-}
-var timer,
-    reloadTimer,
-    resetTimerOnOriginalDelay = !1,
-    slideIndex = 1,
-    slideCount = 0,
-    animationDuration = 1e3,
-    animationSpeed = 300,
-    slideShowDuration = 3e4,
-    trafficBoxShowDuration = 1e4,
-    delay = slideShowDuration,
-    isFirstRun = !0;
+// In diesem Rythmus schiebt der slider die Nachrichten durch 
+const sliderDelay = 12 * 1000;
+// Nach dieser Zeit wird die Seite komplett neu geladen,
+// und somit die Daten neu von SiteForum gefetcht
+const reloadAfter =  10 * 60 * 1000; // 10 Minuten
+
+// Siteforum API
+const verkehrslage_url = "https://abrmd.siteforum.com/de/app/webtools/messages.widget?design=0&navigation=0&action=overview&scheduled=0";
+const baustellen_url = "https://abrmd.siteforum.com/de/app/webtools/messages.widget?design=0&navigation=0&action=overview&scheduled=1";
+
+// Diese Farben stammen aus der Siteforum API, und müssten manuell angepasst werden, falls neue Linien dazu kommen
+const lineColors = {
+  rb75: "#b17f4a",
+  re17: "#36a9e1",
+  rb20: "#95c11f",
+  rb59: "#d47f8f",
+  rb51: "#804a96",
+  re19: "#95c11f",
+  re15: "#2aaae2",
+  re9: "#000",
+  re16: "#2e4c9c",
+  re10: "#52ae32",
+  "30": "#000",
+  re18: "#000",
+  rb44: "#d47f8f",
+  rb25: "#52ae32",
+  rb48: "#2d847e",
+  rb77: "#73542f",
+  re8: "#000",
+  re28: "#000",
+  s7: "#000",
+  rb57: "#000"
+};
+
 
 $("#header-date").clock({ seconds: "false", langSet: "de" }),
 $("#header-time").clock({ seconds: "false", calendar: "false", langSet: "de" });
 
-var parameter = getParameterByName("id") || "auskunft";
-
-
-// Auch das hier erschließt sich mir nicht
-function removePreloader() {
-    $(".preloader").remove();
-}
-
 //---------------------------------------------------------------------------------------------------
 // Ab hier hat Abellio das Zepter wieder selbst in die Hand genommen und gecodet
 //
-
 
 function fetchHtml(url) {
   return fetch(url)
@@ -95,11 +103,9 @@ function parseSiteFormResponse(meldung, raw) {
 
 function renderTrafficCards(data) {
 
-  // Für Verkehrsmeldungen
-  const danger = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#000000"><path d="M480-79q-16 0-30.5-6T423-102L102-423q-11-12-17-26.5T79-480q0-16 6-31t17-26l321-321q12-12 26.5-17.5T480-881q16 0 31 5.5t26 17.5l321 321q12 11 17.5 26t5.5 31q0 16-5.5 30.5T858-423L537-102q-11 11-26 17t-31 6Zm0-361q17 0 28.5-11.5T520-480v-160q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640v160q0 17 11.5 28.5T480-440Zm0 120q17 0 28.5-11.5T520-360q0-17-11.5-28.5T480-400q-17 0-28.5 11.5T440-360q0 17 11.5 28.5T480-320Z"/></svg>`
-  // Für Baustellen
-  const maintenance = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#1f1f1f"><path d="M508.5-331.5Q520-343 520-360t-11.5-28.5Q497-400 480-400t-28.5 11.5Q440-377 440-360t11.5 28.5Q463-320 480-320t28.5-11.5ZM440-440h80v-200h-80v200ZM370-80l-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm40-320Z"/></svg>`
+  // Die beiden folgenden SVGs sind aus der Schienenlounge
 
+  // Für Verkehrsmeldungen
   const troete = `
     <?xml version="1.0" encoding="UTF-8"?>
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250" fill="#1f1f1f">
@@ -119,6 +125,7 @@ function renderTrafficCards(data) {
     </svg>
   `
 
+  // Für Baustellen
   const werkzeug = `
   <?xml version="1.0" encoding="UTF-8"?>
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250" fill="#1f1f1f">
@@ -143,8 +150,6 @@ function renderTrafficCards(data) {
     }
     const key = item.linie.toLowerCase().replace(/\s+/g, "");
     const color = lineColors[key] || "#999"; // fallback
-    console.log(`Karte für Linie ${key} wird gebaut.`)
-
 
     return `
     <div class="traffic-card">
@@ -183,12 +188,11 @@ function renderTrafficCards(data) {
 }
 
 // Ticker slidet alle Elemente in .cards-wrapper (hier .traffic-cards) von rechts nach links
-// das vorne verschwindenede element, wird hinten drangetan
-function startTicker() {
+// das vorne verschwindenede element, wird hinten dran getan
+function startTicker(delay) {
   const wrapper = document.querySelector(".cards-wrapper");
   const cardWidth = wrapper.firstElementChild.offsetWidth + 40; // 40px = gap
 
-  const delay = 12 * 1000 // 15 Sekunden
 
   setInterval(() => {
     wrapper.style.transition = "transform 0.5s ease";
@@ -274,19 +278,13 @@ Promise.all([
     document.querySelector(".preloader")?.remove();
     
     if((pv.length > 1) || (pb.length > 1)){
-      startTicker();
+      startTicker(sliderDelay);
     }
 
   })
   .catch(error => {
     console.error("Fetch error:", error);
 });
-
-// Nach 10 Minuten nen Reload
-const reloadAfter =  10 * 60 * 1000;
-
-// Nach 10 Sekunden nen Reload
-// const reloadAfter =  10 * 1000;
 
 setTimeout(() => {
   window.location.reload();
